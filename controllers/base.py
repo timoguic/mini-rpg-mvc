7ae419df4b04cc57727bb299bff1700d874dccf0
Tim G <timoguic@gmail.com>
2021-03-18 23:32:03 -0700

    Added pygame clock tick in pygame loop

--- controllers/base.py
+++ controllers/base.py
@@ -19,7 +19,9 @@ class PygameController:
 
     def _run_loop(self):
         running = True
+        clock = pygame.time.Clock()
         while running:
+            clock.tick(30)
             for event in pygame.event.get():
                 # Invoked close from the OS
                 if event.type == pygame.locals.QUIT:
